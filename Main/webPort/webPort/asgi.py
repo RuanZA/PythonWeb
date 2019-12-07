d8888c223f0f13ce180de2475ae0ba3fe53709bb
RuanZA <ruanerasmus29@gmail.com>
2019-12-07 15:24:56 +0200

    django start project

--- Main/webPort/webPort/asgi.py
+++ Main/webPort/webPort/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for webPort project.
+ASGI config for webport project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webPort.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webport.settings')
 
 application = get_asgi_application()
